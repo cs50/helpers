6197c6bf002f47f4b8c676636d109f6edffceb82
coderigo17 <daboinsanchez@college.harvard.edu>
2019-07-14 18:01:08 -0400

    updated python.py with separated helpers

--- helpers/python.py
+++ helpers/python.py
@@ -4,9 +4,35 @@ from help50 import helper
 
 
 @helper("python")
-def catch_all_python(lines):
+def address_already_in_use(lines):
     """
-      >>> "environment variable" in catch_all_python([                       \
+      >>> "already listening" in address_already_in_use([  \
+              "Traceback (most recent call last):",        \
+              "OSError: [Errno 98] Address already in use" \
+          ])[1][0]
+      True
+    """
+    # check for recognized output
+    if not re.search(r"^Traceback \(most recent call last\):$", lines[0]):
+        return
+
+    # iterate over lines ourselves
+    for i, line in enumerate(lines):
+
+        matches = re.search(r"^OSError: \[Errno 98\] Address already in use$", line)
+        if matches:
+            response = [
+                "Another program is already listening on that port.",
+                "Do you perhaps have this same command running in another tab?"
+            ]
+
+            return lines[i:i+1], response
+
+
+@helper("python")
+def env_var_not_set(lines):
+    """
+      >>> "environment variable" in env_var_not_set([                        \
               "Traceback (most recent call last):",                          \
               " File \\"./tweets\\", line 45, in <module>",                  \
               "   main()",                                                   \
@@ -17,12 +43,6 @@ def catch_all_python(lines):
               "RuntimeError: API_KEY not set",                               \
               "...",                                                         \
               "RuntimeError: API_SECRET not set"                             \
-          ])[1][0]
-      True
-
-      >>> "already listening" in catch_all_python([        \
-              "Traceback (most recent call last):",        \
-              "OSError: [Errno 98] Address already in use" \
           ])[1][0]
       True
     """
@@ -40,16 +60,6 @@ def catch_all_python(lines):
                     "tab".format(matches.group(1)),
                 "Odds are you need to run `export {}=value`, where `value` is `{}`'s intended " \
                     "value.".format(matches.group(1), matches.group(1))
-            ]
-
-            return lines[i:i+1], response
-
-
-        matches = re.search(r"^OSError: \[Errno 98\] Address already in use$", line)
-        if matches:
-            response = [
-                "Another program is already listening on that port.",
-                "Do you perhaps have this same command running in another tab?"
             ]
 
             return lines[i:i+1], response
